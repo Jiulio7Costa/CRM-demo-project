ab8267f0abe65bbdcc29c9eeff0657375eea4814
Jiulio7Costa <146636254+Jiulio7Costa@users.noreply.github.com>
2024-09-03 14:06:18 +0200

    payment.blade.php

--- cr/resources/views/payment.blade.php
+++ cr/resources/views/payment.blade.php
@@ -1,19 +1,1 @@
-@extends('layouts.app')
 
-@section('content')
-    <h1>Pay Invoice</h1>
-    <p><strong>Number:</strong> {{ $invoice->number }}</p>
-    <p><strong>Amount:</strong> ${{ $invoice->amount }}</p>
-    <form action="{{ route('processPayment', $invoice->id) }}" method="POST">
-        @csrf
-        <script
-            src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-            data-key="{{ env('STRIPE_KEY') }}"
-            data-amount="{{ $invoice->amount * 100 }}"
-            data-name="Laravel Demo"
-            data-description="Payment for Invoice {{ $invoice->number }}"
-            data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-            data-locale="auto">
-        </script>
-    </form>
-@endsection
