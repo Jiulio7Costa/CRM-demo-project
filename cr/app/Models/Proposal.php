b505bd60894aae0429a5da366653ebae9c2abbf4
Jiulio7Costa <146636254+Jiulio7Costa@users.noreply.github.com>
2024-09-12 15:08:15 +0200

    Proposal.php

--- cr/app/Models/Proposal.php
+++ cr/app/Models/Proposal.php
@@ -9,12 +9,16 @@ class Proposal extends Model
 {
     use HasFactory;
 
-    // Allow mass assignment of these attributes
     protected $fillable = [
         'customer_id',
         'title',
         'description',
         'amount',
-        '_token' // This should not be added here. Instead, ensure your form isn't sending the _token in the $request->all() array.
+        'start_date',
+        'end_date',
+        'priority',
+        'status',
+        'contact_info',
+        'comments',
     ];
 }
