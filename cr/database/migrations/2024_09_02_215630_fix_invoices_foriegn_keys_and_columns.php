51f44b78986571e26f377e112104657437973070
Jiulio7Costa <146636254+Jiulio7Costa@users.noreply.github.com>
2024-09-14 16:56:03 +0200

    2024_09_02_215630_fix_invoices_foriegn_keys_and_columns.php

--- cr/database/migrations/2024_09_02_215630_fix_invoices_foriegn_keys_and_columns.php
+++ cr/database/migrations/2024_09_02_215630_fix_invoices_foriegn_keys_and_columns.php
@@ -9,19 +9,15 @@ class FixInvoicesForeignKeysAndColumns extends Migration
     public function up()
     {
         Schema::table('invoices', function (Blueprint $table) {
-            // Example of how to modify a column or add a foreign key
-            // Make sure to replace with actual changes you need
+         
             $table->string('new_column')->nullable(); // Example of adding a new column
-            // $table->foreign('customer_id')->references('id')->on('customers'); // Example of adding a foreign key
         });
     }
 
     public function down()
     {
         Schema::table('invoices', function (Blueprint $table) {
-            // Example rollback
             $table->dropColumn('new_column');
-            // $table->dropForeign(['customer_id']);
         });
     }
 }
